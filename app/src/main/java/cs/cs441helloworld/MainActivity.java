35bdbe932ad99abf4843953b2678cb72b329e6a7
Frankie <email@frankiescott.net>
2019-06-04 16:10:15 -0400

    added fade transition from main activity to 2nd activity

--- app/src/main/java/cs/cs441helloworld/MainActivity.java
+++ app/src/main/java/cs/cs441helloworld/MainActivity.java
@@ -1,14 +1,17 @@
 package cs.cs441helloworld;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -97,7 +101,9 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                getWindow().setExitTransition(new Fade());
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
             }
         });
     }
